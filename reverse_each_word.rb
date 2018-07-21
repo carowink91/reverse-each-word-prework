29c8dcd30720f03af99be64582bd7614de1ad109
angelica winkler <caw400@nyu.edu>
2018-07-21 16:11:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word (sentence)
+  sentence = sentence.split(/ /)
+  sentence.each do |word|
+    word.reverse!
+  end
+  sentence.join(" ")
+end
+
+def reverse_each_word (sentence)
+  sentence = sentence.split(/ /)
+  sentence.collect do |word|
+    word.reverse!
+  end
+  sentence.join(" ")
+end
