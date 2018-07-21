1250a1c8dcabbad1fdbef04dba4c874fcb32e63f
angelica winkler <caw400@nyu.edu>
2018-07-21 16:10:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word (sentence)
   end
   sentence.join(" ")
 end
+
+def reverse_each_word (sentence)
+  sentence = sentence.split(/ /)
+  sentence.collect do |word|
+    word.reverse
+  end
+  sentence.join(" ")
+end
